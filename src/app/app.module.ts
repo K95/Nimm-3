7f3d415472c3ddb25b9cd06e94f4e4a5a3c8b560
k95 <1524381@stud.hs-mannheim.de>
2017-02-07 15:47:00 +0100

    routing (felher im pfad)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routes } from './app.router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -28,7 +28,8 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,3 +2,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from '.login/login.component';
+import { ChangePasswordComponent } from '.change-password/change-password.component';
+
+
+export const router: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'index', redirectTo: 'app', pathMatch: 'full'},
+  { path: 'change-password', redirectTo: 'change-password', pathMatch: 'full'}
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
