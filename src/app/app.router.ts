0e16d89976ce6d9ce5a857413bcef0d033f19ced
k95 <1524381@stud.hs-mannheim.de>
2017-02-13 14:14:15 +0100

    routing funtkioniert

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,15 +1,24 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { LoginComponent } from '.login/login.component';
-import { ChangePasswordComponent } from '.change-password/change-password.component';
+import { LoginComponent } from './login/login.component';
+import { WindowComponent } from './window/window.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { ChangeAvatarComponent } from './change-avatar/change-avatar.component';
+import { DeleteProfileComponent } from './delete-profile/delete-profile.component';
 
-
-export const router: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'index', redirectTo: 'app', pathMatch: 'full'},
-  { path: 'change-password', redirectTo: 'change-password', pathMatch: 'full'}
+const routes: Routes = [
+  { path: '',component: LoginComponent},
+  { path: 'index', component: WindowComponent},
+  { path: 'change-password', component: ChangePasswordComponent},
+  { path: 'change-avatar', component: ChangeAvatarComponent},
+  { path: 'delete-profile', component: DeleteProfileComponent}
 ];
 
-export const routes: ModuleWithProviders = RouterModule.forRoot(router);
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class RoutingModule { }
